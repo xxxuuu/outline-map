288414613d7186f840bfc4ab084322ab8e9ccf5d
GitHub Action <action@github.com>
2022-09-01 13:44:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/xxxuuu/outlinemap/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xxxuuu/outlinemap/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xxxuuu.outlinemap.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xxxuuu.outlinemap.MyBundle
 
 class MyApplicationService {
 
